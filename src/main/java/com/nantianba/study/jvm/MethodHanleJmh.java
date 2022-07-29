421993f601974d0913560bac7123853e5441b36b
nantianba <happypig917@gmail.com>
2022-07-29 09:12:24 +0800

    MethodHanleJmh

--- src/main/java/com/nantianba/study/jvm/MethodHanleJmh.java
+++ src/main/java/com/nantianba/study/jvm/MethodHanleJmh.java
@@ -9,8 +9,17 @@ import org.openjdk.jmh.runner.options.OptionsBuilder;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.concurrent.Callable;
 
+/**
+ * Benchmark                     Mode  Cnt          Score          Error  Units
+ * MethodHanleJmh.lambda        thrpt    3  255052284.436 ±  5939016.607  ops/s
+ * MethodHanleJmh.methodHandle  thrpt    3    1423480.358 ±    97809.205  ops/s
+ * MethodHanleJmh.quick         thrpt    3  256389216.117 ± 17274101.160  ops/s
+ * MethodHanleJmh.reflection    thrpt    3   22018184.487 ± 53689257.394  ops/s
+ */
 @BenchmarkMode(Mode.Throughput)
 @Warmup(iterations = 3, time = 1)
 @Measurement(iterations = 3, time = 4)
@@ -29,37 +38,65 @@ public class MethodHanleJmh {
 
     @Benchmark
     public void methodHandle(Blackhole blackhole) throws Throwable {
-        MethodHandle hello = MethodHandles.lookup().findVirtual(B.class, "hello", MethodType.methodType(void.class)).bindTo(son);
+        int o = getO();
 
-        hello.invokeExact();
+        blackhole.consume(o);
+    }
 
-        blackhole.consume(blackhole);
+    private int getO() throws Throwable {
+        MethodHandle hello = MethodHandles.lookup().findVirtual(B.class, "hello", MethodType.methodType(int.class)).bindTo(son);
+
+        int o = (int) hello.invokeExact();
+        return o;
     }
 
     @Benchmark
     public void reflection(Blackhole blackhole) throws Throwable {
+        Object invoke = getObject();
+
+        blackhole.consume(invoke);
+    }
+
+    private Object getObject() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
         Method hello = B.class.getMethod("hello");
 
-        hello.invoke(son);
-
-        blackhole.consume(blackhole);
+        Object invoke = hello.invoke(son);
+        return invoke;
     }
 
     @Benchmark
     public void quick(Blackhole blackhole) throws Throwable {
-        son.hello();
+        int hello = getHello();
 
-        blackhole.consume(blackhole);
+        blackhole.consume(hello);
     }
+
+    private int getHello() {
+        return son.hello();
+    }
+
+    @Benchmark
+    public void lambda(Blackhole blackhole) throws Throwable {
+        Object consume = consume(son::hello);
+
+        blackhole.consume(consume);
+    }
+
+    private Object consume(Callable<Object> run) throws Throwable{
+        return run.call();
+    }
+
 }
 
 class A {
-    public void hello() {
+    public int hello() {
+        return 2;
     }
 }
 
 class B extends A {
     @Override
-    public void hello() {
+    public int hello() {
+        return 1;
     }
 }
