ed95b5b7195760aeeae4f89716bf9b979dab03f9
ying_zhou <ying_zhou@trip.com>
2023-06-20 17:12:59 +0800

    code fix

--- src/main/java/com/nantianba/study/language/IfTest.java
+++ src/main/java/com/nantianba/study/language/IfTest.java
@@ -1,10 +1,24 @@
 package com.nantianba.study.language;
 
+import java.time.OffsetDateTime;
+import java.util.Optional;
+
 public class IfTest {
     public static void main(String[] args) {
         Object o = 10 % 2 == 0 ? doA() : doB();
+        System.out.println("o = " + o);
 
-        System.out.println(o);
+        System.out.println("*********************************************");
+        Object o1 = Optional.empty().map(i -> doA()).or(() -> Optional.of(doB())).get();
+        System.out.println("o1 = " + o1);
+        System.out.println("*********************************************");
+        Object o2 = Optional.of(1).map(i -> doA()).orElse(doB());
+        System.out.println("o2 = " + o2);
+        System.out.println("*********************************************");
+        Object o3 = Optional.of(1).map(i -> doA()).orElseGet(IfTest::doB);
+
+        System.out.println("o3 = " + o3);
+
     }
 
     private static Object doB() {
