746529c80aeb0fb503aa98b24770c768cd001ef7
nantianba <happypig917@gmail.com>
2023-06-22 19:40:22 +0800

    JDK9

--- src/main/java/com/nantianba/study/feature/jdk11/文件流增强.java
+++ src/main/java/com/nantianba/study/feature/jdk11/文件流增强.java
@@ -1,7 +1,6 @@
 package com.nantianba.study.feature.jdk11;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -13,6 +12,7 @@ public class 文件流增强 {
 
         System.out.println("请输入内容,最后Ctrl+D结束：");
         //将会直到文件流结束才会结束阻塞
+//        transferTo
         System.in.transferTo(outputStream);
         System.out.println("输入结束");
 
@@ -23,10 +23,22 @@ public class 文件流增强 {
 
         System.out.println("*".repeat(20));
 
-        Path tempFile= Files.createTempFile("test", ".txt");
+        Path tempFile = Files.createTempFile("test", ".txt");
         Files.writeString(tempFile, "hello world");
+
+//        readString
         String readString = Files.readString(tempFile);
         System.out.println(readString);
+
+        Path test2 = Files.createTempFile("test2", ".txt");
+        FileReader reader = new FileReader(tempFile.toFile());
+        FileWriter writer = new FileWriter(test2.toFile());
+        BufferedReader bufferedReader = new BufferedReader(reader);
+        try (bufferedReader; reader; writer) {
+            //transferTo
+            long transferTo = bufferedReader.transferTo(writer);
+            System.out.println("transferTo = " + transferTo);
+        }
 
 
     }
