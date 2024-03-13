7d86c348a26c79799e8786cd2d53b0fc9f6bba01
ying_zhou <ying_zhou@trip.com>
2024-03-13 19:09:10 +0800

    抽得 fix

--- src/main/java/com/nantianba/study/feature/jdk21/死锁2.java
+++ src/main/java/com/nantianba/study/feature/jdk21/死锁2.java
@@ -3,6 +3,7 @@ package com.nantianba.study.feature.jdk21;
 import java.lang.reflect.InvocationTargetException;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.locks.Lock;
 
 public class 死锁2 {
 
@@ -12,16 +13,35 @@ public class 死锁2 {
         int i1 = 10000;
         Thread[] futures = new Thread[i1];
         AtomicLong atomicLong = new AtomicLong();
+        Lock lock = new java.util.concurrent.locks.ReentrantLock();
 
         for (int i = 0; i < i1; i++) {
-            int finalI = i;
+            int finalI1 = i;
             Thread thread = Thread.startVirtualThread(() -> {
                 synchronized (死锁2.class) {
                     System.out.println("enter");
                     queue.poll();
+
                     try {
-//                        死锁2.class.wait(10);
-                        Thread.sleep(10);
+                        lock.lock();
+                        if (finalI1 == 1000) {
+                            try {
+                                Thread.sleep(6000);
+                            } catch (InterruptedException e) {
+                                System.out.println("interrupt:" + Thread.currentThread().isInterrupted());
+                                Thread.currentThread().interrupt();
+                            }
+                            dead();
+                        }
+                    } finally {
+                        lock.unlock();
+                        if (finalI1 == 1000) {
+                            System.out.println("hhhhhh");
+                        }
+                    }
+                    try {
+                        死锁2.class.wait(10);
+//                        Thread.sleep(10);
                     } catch (InterruptedException e) {
                     }
                 }
@@ -32,6 +52,8 @@ public class 死锁2 {
         }
 
         System.out.println(Thread.activeCount());
+        Thread.sleep(3000);
+        futures[1000].interrupt();
         for (int i = 0; i < i1; i++) {
             futures[i].join();
         }
@@ -44,5 +66,9 @@ public class 死锁2 {
         System.out.println(Thread.activeCount());
         System.out.println(atomicLong.get());
         System.exit(0);
+    }
+
+    private static void dead() {
+        dead();
     }
 }
