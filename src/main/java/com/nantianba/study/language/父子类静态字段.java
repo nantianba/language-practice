4d6f9957f818b8bc7a3d05d576a834d7d3229013
ying_zhou <ying_zhou@trip.com>
2024-03-22 18:09:13 +0800

    code fix

---
+++ src/main/java/com/nantianba/study/language/父子类静态字段.java
@@ -0,0 +1,45 @@
+package com.nantianba.study.language;
+
+import java.lang.reflect.Field;
+
+public class 父子类静态字段 {
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+        System.out.println(Child.name);
+        System.out.println(Child.name2);
+        System.out.println(Parent.name);
+
+
+        System.out.println("*******************");
+
+        {
+            Field name = Parent.class.getField("name");
+            System.out.println(name.get(Child.class));
+
+        }
+        {
+            Field name = Child.class.getField("name");
+            System.out.println(name.get(Child.class));
+        }
+        System.out.println("*******************");
+        System.out.println(Parent.getNameP());
+        System.out.println(Child.getNameP());
+
+    }
+
+    static class Parent {
+        public final static String name = "parent";
+        public final static String name2 = "parent";
+
+        public static String getNameP() {
+            return name;
+        }
+    }
+
+    static class Child extends Parent {
+        public final static String name = "child";
+
+        public static String getNameP() {
+            return name;
+        }
+    }
+}
